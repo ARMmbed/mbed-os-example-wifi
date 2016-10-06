1e037e1ed018dcfe2e8a74017d3d87a92b983d54
Bartek Szatkowski <bulislaw@linux.com>
2016-10-06 13:13:20 +0100

    Add WiFi demo for Ublox Odin and ESP2866

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# mbed-os-example-wifi
+# mbed-os-example-wifi #
+
 Wi-fi example for mbed OS 5.0
+
+## Getting started with the WiFi API ##
+
+This is a quick example of a simple WiFi application using the WiFi and
+network-socket APIs that is provided as a part of [mbed-os](github.com/armmbed/mbed-os).
+
+The program brings up the WiFi and the underlying network interface, and uses it to
+scans available networks, connects to a network, prints interface and connection details
+and performs simple HTTP operation.
+
+### Supported hardware ###
+
+* UBLOX Odin board (UBLOX_EVK_ODIN_W2)
+* ESP2866 module (Board it's connected to shouldn't have other network interface eg. Ethernet)
+
+ESP2866 is a fallback option and will be used if the build is for unsupported platform.
+
+## Configuration ##
+
+``` bash
+-DMBED_DEMO_WIFI_SSID=ssid  # ssid
+-DMBED_DEMO_WIFI_PASS=pass  # passphrase
+```
+
+## Documentation ##
+
+More information on the network-socket API can be found in the [mbed handbook](https://docs.mbed.com/docs/mbed-os-api-reference/en/5.2/APIs/communication/network_sockets/).
---
+++ main.cpp
@@ -0,0 +1,123 @@
+/* WiFi Example
+ * Copyright (c) 2016 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "mbed.h"
+#include "TCPSocket.h"
+
+#if TARGET_UBLOX_EVK_ODIN_W2
+    #include "OdinWiFiInterface.h"
+    OdinWiFiInterface wifi;
+#else
+    #include "ESP8266Interface.h"
+    ESP8266Interface wifi(D1, D0);
+#endif
+
+#define STR(x) STR2(x)
+#define STR2(x) #x
+
+const char *sec2str(nsapi_security_t sec)
+{
+    switch (sec) {
+        case NSAPI_SECURITY_NONE:
+            return "None";
+        case NSAPI_SECURITY_WEP:
+            return "WEP";
+        case NSAPI_SECURITY_WPA:
+            return "WPA";
+        case NSAPI_SECURITY_WPA2:
+            return "WPA2";
+        case NSAPI_SECURITY_WPA_WPA2:
+            return "WPA/WPA2";
+        case NSAPI_SECURITY_UNKNOWN:
+        default:
+            return "Unknown";
+    }
+}
+
+void scan_demo(WiFiInterface *wifi)
+{
+    WiFiAccessPoint *ap;
+
+    printf("Scan:\r\n");
+
+    int count = wifi->scan(NULL,0);
+
+    /* Limit number of network arbitrary to 15 */
+    count = count < 15 ? count : 15;
+
+    ap = new WiFiAccessPoint[count];
+    count = wifi->scan(ap, count);
+    for (int i = 0; i < count; i++)
+    {
+        printf("Network: %s secured: %s BSSID: %hhX:%hhX:%hhX:%hhx:%hhx:%hhx RSSI: %hhd Ch: %hhd\r\n", ap[i].get_ssid(),
+               sec2str(ap[i].get_security()), ap[i].get_bssid()[0], ap[i].get_bssid()[1], ap[i].get_bssid()[2],
+               ap[i].get_bssid()[3], ap[i].get_bssid()[4], ap[i].get_bssid()[5], ap[i].get_rssi(), ap[i].get_channel());
+    }
+    printf("%d networks available.\r\n", count);
+
+    delete[] ap;
+}
+
+void http_demo(NetworkInterface *net)
+{
+    TCPSocket socket;
+
+    printf("Sending HTTP request to www.arm.com...\r\n");
+
+    // Open a socket on the network interface, and create a TCP connection to www.arm.com
+    socket.open(net);
+    socket.connect("www.arm.com", 80);
+
+    // Send a simple http request
+    char sbuffer[] = "GET / HTTP/1.1\r\nHost: www.arm.com\r\n\r\n";
+    int scount = socket.send(sbuffer, sizeof sbuffer);
+    printf("sent %d [%.*s]\r\n", scount, strstr(sbuffer, "\r\n")-sbuffer, sbuffer);
+
+    // Recieve a simple http response and print out the response line
+    char rbuffer[64];
+    int rcount = socket.recv(rbuffer, sizeof rbuffer);
+    printf("recv %d [%.*s]\r\n", rcount, strstr(rbuffer, "\r\n")-rbuffer, rbuffer);
+
+    // Close the socket to return its memory and bring down the network interface
+    socket.close();
+}
+
+int main()
+{
+    printf("WiFi example\r\n\r\n");
+
+    scan_demo(&wifi);
+
+    printf("\r\nConnecting...\r\n");
+    int ret = wifi.connect(STR(MBED_DEMO_WIFI_SSID), STR(MBED_DEMO_WIFI_PASS), NSAPI_SECURITY_WPA_WPA2);
+    if (ret != 0) {
+        printf("\r\nConnection error\r\n");
+        return -1;
+    }
+
+    printf("Success\r\n\r\n");
+    printf("MAC: %s\r\n", wifi.get_mac_address());
+    printf("IP: %s\r\n", wifi.get_ip_address());
+    printf("Netmask: %s\r\n", wifi.get_netmask());
+    printf("Gateway: %s\r\n", wifi.get_gateway());
+    printf("RSSI: %d\r\n\r\n", wifi.get_rssi());
+
+    http_demo(&wifi);
+
+    wifi.disconnect();
+
+    printf("\r\nDone\r\n");
+}
