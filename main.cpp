54210d4336183c8827cc6627c4d8ab9e66c2f307
Alan Chuang <Alan.Chuang@arm.com>
2018-07-12 09:49:53 +0800

    Add easy-connect library Modify example-wifi program to use easy-connect library instead. Modify config files for WISE-1530

--- main.cpp
+++ main.cpp
@@ -16,90 +16,9 @@
 
 #include "mbed.h"
 #include "TCPSocket.h"
+#include "easy-connect.h"
 
-#define WIFI_ESP8266    1
-#define WIFI_IDW0XX1    2
-#define WIFI_ISM43362   3
-
-#if TARGET_UBLOX_EVK_ODIN_W2
-#include "OdinWiFiInterface.h"
-OdinWiFiInterface wifi;
-
-#elif TARGET_REALTEK_RTL8195AM
-#include "RTWInterface.h"
-RTWInterface wifi;
-
-#else // External WiFi modules
-
-#if MBED_CONF_APP_WIFI_SHIELD == WIFI_ESP8266
-#include "ESP8266Interface.h"
-ESP8266Interface wifi(MBED_CONF_APP_WIFI_TX, MBED_CONF_APP_WIFI_RX);
-
-#elif MBED_CONF_APP_WIFI_SHIELD == WIFI_ISM43362
-#include "ISM43362Interface.h"
-ISM43362Interface wifi;
-
-#elif MBED_CONF_APP_WIFI_SHIELD == WIFI_IDW0XX1
-#include "SpwfSAInterface.h"
-SpwfSAInterface wifi(MBED_CONF_APP_WIFI_TX, MBED_CONF_APP_WIFI_RX);
-
-#endif // MBED_CONF_APP_WIFI_SHIELD == WIFI_IDW0XX1
-
-#endif
-
-const char *sec2str(nsapi_security_t sec)
-{
-    switch (sec) {
-        case NSAPI_SECURITY_NONE:
-            return "None";
-        case NSAPI_SECURITY_WEP:
-            return "WEP";
-        case NSAPI_SECURITY_WPA:
-            return "WPA";
-        case NSAPI_SECURITY_WPA2:
-            return "WPA2";
-        case NSAPI_SECURITY_WPA_WPA2:
-            return "WPA/WPA2";
-        case NSAPI_SECURITY_UNKNOWN:
-        default:
-            return "Unknown";
-    }
-}
-
-int scan_demo(WiFiInterface *wifi)
-{
-    WiFiAccessPoint *ap;
-
-    printf("Scan:\n");
-
-    int count = wifi->scan(NULL,0);
-
-    if (count <= 0) {
-        printf("scan() failed with return value: %d\n", count);
-        return 0;
-    }
-
-    /* Limit number of network arbitrary to 15 */
-    count = count < 15 ? count : 15;
-
-    ap = new WiFiAccessPoint[count];
-    count = wifi->scan(ap, count);
-
-    if (count <= 0) {
-        printf("scan() failed with return value: %d\n", count);
-        return 0;
-    }
-
-    for (int i = 0; i < count; i++) {
-        printf("Network: %s secured: %s BSSID: %hhX:%hhX:%hhX:%hhx:%hhx:%hhx RSSI: %hhd Ch: %hhd\n", ap[i].get_ssid(),
-               sec2str(ap[i].get_security()), ap[i].get_bssid()[0], ap[i].get_bssid()[1], ap[i].get_bssid()[2],
-               ap[i].get_bssid()[3], ap[i].get_bssid()[4], ap[i].get_bssid()[5], ap[i].get_rssi(), ap[i].get_channel());
-    }
-    printf("%d networks available.\n", count);
-
-    delete[] ap;
-    return count;
-}
+NetworkInterface *network;
 
 void http_demo(NetworkInterface *net)
 {
@@ -153,7 +72,6 @@ void http_demo(NetworkInterface *net)
 
 int main()
 {
-    int count = 0;
 
     printf("WiFi example\n");
 
@@ -161,29 +79,21 @@ int main()
     printf("Mbed OS version %d.%d.%d\n\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
 #endif
 
-    count = scan_demo(&wifi);
-    if (count == 0) {
-        printf("No WIFI APNs found - can't continue further.\n");
+    network = easy_connect(true, MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD);
+    if (!network) {
+        printf("Connecting to the network failed .... See serial output.\n");
         return -1;
     }
-
-    printf("\nConnecting to %s...\n", MBED_CONF_APP_WIFI_SSID);
-    int ret = wifi.connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
-    if (ret != 0) {
-        printf("\nConnection error: %d\n", ret);
-        return -1;
-    }
-
     printf("Success\n\n");
-    printf("MAC: %s\n", wifi.get_mac_address());
-    printf("IP: %s\n", wifi.get_ip_address());
-    printf("Netmask: %s\n", wifi.get_netmask());
-    printf("Gateway: %s\n", wifi.get_gateway());
-    printf("RSSI: %d\n\n", wifi.get_rssi());
+    printf("MAC: %s\n", network->get_mac_address());
+    printf("IP: %s\n", network->get_ip_address());
+    printf("Netmask: %s\n", network->get_netmask());
+    printf("Gateway: %s\n", network->get_gateway());
+    //printf("RSSI: %d\n\n", wifi->get_rssi();
 
-    http_demo(&wifi);
+    http_demo(network);
 
-    wifi.disconnect();
+    network->disconnect();
 
     printf("\nDone\n");
 }
